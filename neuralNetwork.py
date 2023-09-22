4e56691fcbd55a948a376992de1492756deadaa7
ZoranUTF8 <janjiczoran23@gmail.com>
2023-09-22 23:51:28 +0900

    Add init with weights

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,14 +1,29 @@
 # neural network class definition
+import numpy
+
+
 class neuralNetwork:
 
     # initialise the neural network
-    def __init__(self):
-        pass
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        # set number of nodes in each input, hidden, output layer
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+        # learning rate
+        self.learningrate = learningrate  # link weight matrices, wih and who
+        # weights inside the arrays are w_i_j, where link is from node i to node j in the next layer
+        # w11 w21
+        # w12 w22 etc
+        self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
 
-    # train the neural network
-    def train(self):
-        pass
 
-    # query the neural network
-    def query(self):
-        pass
+# train the neural network
+def train(self):
+    pass
+
+
+# query the neural network
+def query(self):
+    pass
