2dd172b66aec69005f239cbe6e42df929d529a20
ZoranUTF8 <janjiczoran23@gmail.com>
2023-10-19 23:00:25 +0900

    rescale training data

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
-
-
 from neuralNetwork import neuralNetwork
 import numpy
 import matplotlib.pyplot
-#number of input, hidden and output
+
+# number of input, hidden and output
 # input_nodes= 3
 # hidden_nodes = 3
 # output_nodes = 3
@@ -20,11 +19,8 @@ data_file = open("data/mnist_train_100.csv", 'r')
 data_list = data_file.readlines()
 data_file.close()
 
-all_values = data_list[0].split(',')
-image_array = numpy.asfarray(all_values[1:]).reshape((28,28))
-
-matplotlib.pyplot.imshow(image_array, cmap='Greys', interpolation='None')
+all_values = data_list[1].split(',')
+scaled_input = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+print(scaled_input)
 matplotlib.pyplot.savefig("output/samplePlot2.png")
 
-
-print(data_list[0])
